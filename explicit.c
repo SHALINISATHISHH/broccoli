d62b0a933068472b8db9d10a0356fd90b6c20e45
SHALINISATHSIHH <shalinisadhish368@gmail.com>
2025-02-05 15:27:33 +0530

    ver5

---
+++ explicit.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main(){
+    int num1,num2,num3;
+    float avg;
+    num1=10;
+    num2=20;
+    num3=30;
+    avg=(float)(num1+num2+num3)/3;
+    printf("average=%f",avg);
+    return 0;
+}
