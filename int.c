6479e885da94d541297ae64832ffeb6e4be90ac4
SHALINISATHSIHH <shalinisadhish368@gmail.com>
2025-02-05 16:19:50 +0530

    ver6

--- int.c
+++ int.c
@@ -3,6 +3,6 @@ int main(){
     int var_a;
     printf("enter a number:");
     scanf("%d",&var_a);
-   printf("entered number is %d",var_a);
+   printf("entered  is %d",var_a);
     return 0;
 }
--- number.c
+++ number.c
@@ -4,6 +4,6 @@ int main(){
     long long int number;
     printf("enter a number:");
     scanf("%lld",&number);
-    printf("entered number is %lld",number);
+   printf("entered number is %lld",number);
     return 0;
 }
