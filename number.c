922023c708921c5fb23cdf1ed937f3b85767ca2d
SHALINISATHSIHH <shalinisadhish368@gmail.com>
2025-02-05 15:05:34 +0530

    ver3

---
+++ number.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+int main(){
+    
+    long long int number;
+    printf("enter a number:");
+    scanf("%lld",&number);
+    printf("entered number is %lld",number);
+    return 0;
+}
